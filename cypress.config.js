f1b5a365ae0cd418beadd541f4fba66376806d9b
Israel Faioli <68829837+israfaioli@users.noreply.github.com>
2024-04-03 16:20:36 -0300

    criando pipeline no github actions

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,10 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://notes-serverless-app.com',
-    defaultCommandTimeout: 30000,
+    env: {
+      viewportWidthBreakpoint: 768,
+    },
+    defaultCommandTimeout: 60000,
     chromeWebSecurity: false,
     setupNodeEvents(on, config) {
     },
